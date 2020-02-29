687188926b1d63f02a6353728b22f71dd8622b55
AlinaPopescu1 <58535557+AlinaPopescu1@users.noreply.github.com>
2020-02-29 15:49:23 -0500

    Update CancelReservation.java

--- src/test/CancelReservation.java
+++ src/test/CancelReservation.java
@@ -2,6 +2,8 @@ package test;
 
 public class CancelReservation {
     public static void main(String[] args) {
-
+       int a = Integer.valueOf("100");
+       String name="Alina";
+        
     }
 }
